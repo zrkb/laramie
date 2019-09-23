6b78502f0df50254c7ce1e1336c22b1d8903bc78
Felix Ayala <felix1262@gmail.com>
2019-09-23 17:53:51 -0400

    Fix imagesPath call in SingleImage

--- resources/views/fields/single-image-field/index.blade.php
+++ resources/views/fields/single-image-field/index.blade.php
@@ -1,6 +1,6 @@
 <div class="avatar avatar-sm">
 	@if ($image = $item->getAttribute($field->attribute))
-		<img src="{{ Storage::url($resource->model()::imagesPath($image->basename)) }}" alt="{{ $item->name }}" class="avatar-img rounded">
+		<img src="{{ Storage::url($item::imagesPath($image->basename)) }}" alt="{{ $item->name }}" class="avatar-img rounded">
 	@else
 		<img src="/backend/assets/img/icon-image.svg" alt="..." class="avatar-img">
 	@endif
